de86e8decb4ddcd6300658b91bd3bc0aec1cbffe
Angelo Sobzinski <sobzinskia@gmail.com>
2026-09-22 10:48:20 -0300

    Testa verificador com outra idade

---
+++ Verificadormaioridade/Maioridade.js
@@ -0,0 +1,9 @@
+const nome = "Pedro";
+const idade = 14;
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+if (idade >= 18) {
+console.log("Situação: maior de idade");
+} else {
+console.log("Situação: menor de idade");
+}
