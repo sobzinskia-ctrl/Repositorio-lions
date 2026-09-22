af182f6ece7337f9ebe11a0b23f28f23a76210e4
Angelo Sobzinski <sobzinskia@gmail.com>
2026-09-22 09:50:20 -0300

    Primeiro commit: adiciona lista de presença

---
+++ listaPresenca.js
@@ -0,0 +1,8 @@
+const alunos = ["Ana", "Bruno", "Carla"];
+console.log("Lista de Presença");
+console.log("------------------");
+for (let i = 0; i < alunos.length; i++) {
+console.log((i + 1) + ". " + alunos[i]);
+}
+console.log("------------------");
+console.log("Total de alunos: " + alunos.length);
