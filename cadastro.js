67d5ccd3caf0b13a2381048cee77bf787e784bde
Angelo Sobzinski <sobzinskia@gmail.com>
2026-09-23 08:40:34 -0300

    Adiciona Cadastro no Aluno

---
+++ cadastro.js
@@ -0,0 +1,10 @@
+let aluno ={
+    nome: "Angelo",
+    idade: 23,
+    curso: "Javascript",
+    ativo: true
+}
+
+console.log("Nome: " + aluno.nome)
+console.log("Idade: " + aluno.idade)
+console.log("Curso: " + aluno.curso)
